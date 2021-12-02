57edcd015545c8c3cb5eb895982f97c2cef358b0
eunnnnn1996 <eunnnnn1996@gmail.com>
2021-12-02 12:02:59 +0900

    21/12/02 12:

--- ch15-jdbc/src/kr/s04/preparedstatement/UpdateMemo.java
+++ ch15-jdbc/src/kr/s04/preparedstatement/UpdateMemo.java
@@ -8,46 +8,47 @@ import java.sql.ResultSet;
 
 import kr.s03.preparedstatement.DBUtill;
 
-public class SelectDetailMemo {
+public class UpdateMemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BufferedReader br = null;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql = null;
-		
+		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("메모 상세페이지를 조회합니다.");
+			System.out.println("메모를 수정합니다.");
 			System.out.println("메모 번호>");
 			int num = Integer.parseInt(br.readLine());
+			System.out.println("제목");
+			String title = br.readLine();
+			System.out.println("이름");
+			String name = br.readLine();
+			System.out.println("메모");
+			String memo = br.readLine();
+			System.out.println("이메일");
+			String email = br.readLine();
 			
 			conn = DBUtill.getConnection();
-			
-			sql = "select * from test2 where num = ?";
+			sql = "update test2 set title = ? , name = ? , memo = ? , email = ?  where num =?";
 			
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1,num);
 			
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				System.out.println("번호 : "+ rs.getInt(num));
-				System.out.println("제목 : "+ rs.getString("title"));
-				System.out.println("작성자 : "+ rs.getString("name"));
-				System.out.println("내용 : "+ rs.getString("memo"));
-				System.out.println("이메일 : "+ rs.getString("email"));
-				System.out.println("작성일 : "+ rs.getDate("reg_date"));
-			}else {
-				System.out.println("데이터 없음");
-			}
+			pstmt.setString(1, title);
+			pstmt.setString(2, name);
+			pstmt.setString(3, memo);
+			pstmt.setString(4, email);
+			pstmt.setInt(5, num);
+			
+			int count = pstmt.executeUpdate();
+			System.out.println(count + "개 행의 정보를 수정했습니다.");
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}finally {
 			DBUtill.executeClose(rs, pstmt, conn);
-			if(br!=null)try {br.close();}catch(Exception e) {}
 		}
 	}
 
