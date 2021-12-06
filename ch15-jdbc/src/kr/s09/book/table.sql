d225a2fe4c97d6a0b074461969c40c2cddf07e90
eunnnnn1996 <eunnnnn1996@gmail.com>
2021-12-06 20:59:52 +0900

    21/12/06 9:

--- ch15-jdbc/src/kr/s09/book/table.sql
+++ ch15-jdbc/src/kr/s09/book/table.sql
@@ -1,7 +1,7 @@
 create table member{
 me_num number primary key,
 me_id varchar2(10) unique not null,
-me_passwd varchar(10) not null,
+me_passwd varchar2(10) not null,
 me_name varchar2(30) not null,
 me_phone varchar2(13) not null,
 me_regdate date default SYSDATE not null
